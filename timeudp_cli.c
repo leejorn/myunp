7cc416ac36419ea91e2e47f25281b96be673ec89
leejorn <lijizhan@126.com>
2018-04-30 10:52:01 +0800

    &len fuck error

--- timeudp_cli.c
+++ timeudp_cli.c
@@ -18,16 +18,55 @@ int main(int argc, char **argv)
 	ser_addr.sin_port = htons(10001);
 	inet_pton(AF_INET, argv[1], &ser_addr.sin_addr);
 
-	sock_cli = socket(PF_INET, SOCK_DGRAM, 0);
+	sock_cli = socket(AF_INET, SOCK_DGRAM, 0);
 
 	while(fgets(sendline, 1024, stdin) != NULL)
 	{
 		sendto(sock_cli, sendline, strlen(sendline), 0, (struct sockaddr *)&ser_addr, sizeof(ser_addr));
+		printf("finish send\n");
 		n = recvfrom(sock_cli, recvline, 1024, 0, NULL, NULL);
+		printf("get back : %d\n", n);
 		recvline[n] = 0;
-		printf("get back : %s\n", recvline);
 		//fputs(recvline, stdout);
 	}
 
 	return 0;
 }
+
+/*
+void dg_cli(FILE *fp, int sockfd, const struct sockaddr *pservaddr, socklen_t servlen)  
+{ 
+	int n;  
+	char sendline[1024], recvline[1024 + 1];  
+
+	//从终端获取一行字符串，将其写入套接字    
+    	//然后从套接字一行字符串，将其写入终端    
+ 	while (fgets(sendline, 1024, fp) != NULL) 
+	{  
+		sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);  
+		n = recvfrom(sockfd, recvline, 1024, 0, NULL, NULL);  
+		recvline[n] = 0;  
+		fputs(recvline, stdout);  
+	}  
+}
+
+int main(int argc, char **argv)  
+{  
+	int  sockfd;  
+	struct sockaddr_in  servaddr;  
+
+	if (argc != 2)  
+		printf("usage: udpcli <IPaddress>");  
+
+	//创建用于TCP协议的套接字   
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);  
+	memset(&servaddr, 0, sizeof(servaddr));  
+	servaddr.sin_family = AF_INET;  
+	servaddr.sin_port = htons(10001);  
+
+	//将程序的参数1(argv[1])转换成套接字地址结构    
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);  
+	dg_cli(stdin, sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));  
+	return 0;  
+}  
+*/
--- timeudp_ser.c
+++ timeudp_ser.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 
+
 char sermsg[] = "welcome to my room !!\n";
 
 int main(int argc, char **argv)
@@ -18,7 +19,7 @@ int main(int argc, char **argv)
 	ser_addr.sin_port = htons(10001);
 	ser_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	sock_ser = socket(PF_INET, SOCK_DGRAM, 0);
+	sock_ser = socket(AF_INET, SOCK_DGRAM, 0);
 
 	bind(sock_ser, (struct sockaddr *)&ser_addr, sizeof(ser_addr));
 
@@ -29,12 +30,53 @@ int main(int argc, char **argv)
 		socklen_t len;
 		int n;
 
+		len = sizeof(cli_addr);
 		n = recvfrom(sock_ser, climsg, 1024, 0, (struct sockaddr *)&cli_addr, &len);
 		climsg[n] = 0;
 		printf("recv msg : %s\n", climsg);
 
-		sendto(sock_ser, sermsg, strlen(sermsg), 0, (struct sockaddr *)&cli_addr, len);
+		sendto(sock_ser, climsg, n, 0, (struct sockaddr *)&cli_addr, len);
+
+		printf("send msg\n");
 	}
 
 	return 0;
 }
+
+/*
+void dg_echo(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen)
+{  
+	int n;  
+	socklen_t len;  
+	char mesg[1024];
+
+	//每当一个数据报到达时，先接收数据，然后再把数据返回给客户端 
+	while( 1 )  
+	{  
+		len = clilen;  
+		n = recvfrom(sockfd, mesg, 1024, 0, pcliaddr, &len);  
+		sendto(sockfd, mesg, n, 0, pcliaddr, len);  
+	} 
+
+	return;
+}  
+
+int main(int argc, char **argv)  
+{ 
+	int sockfd;  
+	struct sockaddr_in servaddr, cliaddr;  
+
+	//建立一个基于IPv4数据报(udp)的套接字  
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);  
+	memset(&servaddr, 0, sizeof(servaddr));  
+	servaddr.sin_family = AF_INET;  
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+	servaddr.sin_port = htons(10001);  
+
+	//把socket和socket地址结构联系起来  
+	bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));  
+	dg_echo(sockfd, (struct sockaddr *) &cliaddr, sizeof(cliaddr));  
+						
+	return 0;  
+}  
+*/
